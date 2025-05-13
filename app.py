748091c4cb9b90483638316ea0a9b7a31cf71718
Satvik19120 <satvik11042006@gmail.com>
2025-05-13 14:51:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    insta_url = "https://www.instagram.com/coderbaba7/"
+    youtube = "https://www.youtube.com/@coderbaba7"
     if request.method == 'POST':
-        # Get the ER number and DOB from the form
+        # Get the ER number
         er_no = request.form['er_no']
         
         admit_card_url = f'https://bteup.ac.in/ESeva/Student/AdmitCard.aspx?EnrollNumber={er_no}'
         verification_card_url = f'https://bteup.ac.in/ESeva/Student/VerificationCard.aspx?EnrollNumber={er_no}'
-        insta_url = "https://www.instagram.com/coderbaba7/"
-        youtube = "https://www.youtube.com/@coderbaba7"
         return render_template('index.html', admit_card_url=admit_card_url, verification_card_url=verification_card_url, er_no=er_no,insta_url=insta_url, youtube=youtube)
         
         
-    return render_template('index.html', admit_card_url=None, verification_card_url=None, error=None,insta_url=None, youtube=None)
+    return render_template('index.html', admit_card_url=None, verification_card_url=None, error=None,insta_url=insta_url, youtube=youtube)
 
 if __name__ == '__main__':
     app.run(debug=True)
